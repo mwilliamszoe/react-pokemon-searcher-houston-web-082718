a00f6ae30da324d14925608f4a98b00960cb5bfb
github username <github email address>
2018-11-13 17:58:13 -0600

    im done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,44 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  state = {
+    img: this.props.pokemon.sprites.front
+  };
+
+  toggleImg = () => {
+    const { pokemon } = this.props;
+    this.state.img === pokemon.sprites.front
+      ? this.setState({ img: pokemon.sprites.back })
+      : this.setState({ img: pokemon.sprites.front });
+  };
+
   render() {
+    const { name, stats } = this.props.pokemon;
+
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img alt="oh no!" onClick={this.toggleImg} src={this.state.img} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {stats.map(stat => {
+                if (stat.name === "hp") {
+                  return stat.value;
+                }
+              })}
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemonList: [],
+    filteredList: []
+  };
+
+  componentDidMount = () => {
+    fetch(" http://localhost:3000/pokemon")
+      .then(res => res.json())
+      .then(pokemonList =>
+        this.setState({
+          pokemonList: pokemonList,
+          filteredList: pokemonList
+        })
+      );
+  };
+
+  searchPokemon = event => {
+    const desiredPokemon = this.state.pokemonList.filter(pokemon => {
+      return pokemon.name.includes(event.target.value);
+    });
+    this.setState({
+      filteredList: desiredPokemon
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.searchPokemon} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection filteredList={this.state.filteredList} />
         <br />
         <PokemonForm />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
